21955b34020362be96c5498a0e47786881ec3eb3
Maciej Druciarek <kobraks2@gmail.com>
2022-07-18 12:11:33 +0200

    Added missed files to premake

--- premake5.lua
+++ premake5.lua
@@ -3,26 +3,32 @@ project "GLFW"
 	language "C"
 	staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+
+		"src/internal.h",
+		"src/platform.h",
+		"src/mappings.h",
+		
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-
+		"src/platform.c",
+		"src/vulkan.c",
+		"src/window.c",
+		
+		"src/null_platform.h",
+		"src/null_joystick.h",		
 		"src/null_init.c",
-		"src/null_joystick.c",
 		"src/null_monitor.c",
 		"src/null_window.c",
-
-		"src/platform.c",
-		"src/window.c",
+		"src/null_joystick.c",
 	}
 
 	filter "system:linux"
@@ -32,16 +38,30 @@ project "GLFW"
 		
 		files
 		{
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
+			
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
+			
 			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+			
+			"src/posix_poll.h",
+			"src/posix_poll.c",
+			
+			"src/linux_joystick.h",
+			"src/linux_joystick.c",
 		}
 
 		defines
@@ -54,16 +74,23 @@ project "GLFW"
 
 		files
 		{
+			"src/win32_time.h",
+			"src/win32_thread.h",
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			
+			"src/win32_module.c",
+			"src/win32_thread.c",
+			"src/win32_time.c",
+			
 			"src/win32_init.c",
 			"src/win32_joystick.c",
-			"src/win32_module.c",
 			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
+		
 			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/wgl_context.c",
+			"src/osmesa_context.c",
 		}
 
 		defines 
